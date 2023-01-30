4b661177e55c3c252e9f9bce4f333bca42970023
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-01-30 18:36:33 +0900

    Coin Tracker - challenge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState("0");
+  const [select, SetSelect] = useState("1");
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then(response => response.json())
@@ -12,19 +14,29 @@ function App() {
       });
   }, []);
 
+  const onChange = event => setUsd(event.target.value);
+  const setSelectCoin = event => SetSelect(event.target.value);
+
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
-          {coins.map(coin => (
-            <option>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
+        <div>
+          <select onChange={setSelectCoin}>
+            {coins.map((coin, index) => (
+              <option key={index} value={coin.rank}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <div>
+            <span>USD:</span>
+            <input onChange={onChange} value={usd} type="number" />
+            <h3>Coins: {usd / coins[select - 1].quotes.USD.price}</h3>
+          </div>
+        </div>
       )}
     </div>
   );
