bc3ebe0bf1fce39e6fec6bec4216bf2f7bcf7488
seunghyeokleeme <seunghyeok.tech@gmail.com>
2023-01-30 21:44:20 +0900

    movie app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,8 @@
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [usd, setUsd] = useState("0");
-  const [select, SetSelect] = useState("1");
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then(response => response.json())
-      .then(json => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
-  const onChange = event => setUsd(event.target.value);
-  const setSelectCoin = event => SetSelect(event.target.value);
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <div>
-          <select onChange={setSelectCoin}>
-            {coins.map((coin, index) => (
-              <option key={index} value={coin.rank}>
-                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-          <div>
-            <span>USD:</span>
-            <input onChange={onChange} value={usd} type="number" />
-            <h3>Coins: {usd / coins[select - 1].quotes.USD.price}</h3>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
